07201930d54a6f6f8175214b68443f61e3960410
Denis <159953887+DBodies@users.noreply.github.com>
2024-12-19 17:08:39 +0200

    probably last fix

--- src/main.js
+++ src/main.js
@@ -2,15 +2,16 @@
 
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[open-menu]"),
-    
-    closeModalBtn: document.querySelector("[menu-close]"),
-    modal: document.querySelector("[menu]"),
+    openMenuBtn: document.querySelector("[data-open-menu]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    menu: document.querySelector("[data-menu]"),
   };
-  refs.openModalBtn.addEventListener("click", toggleModal);
- 
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-  function toggleModal() {
-    refs.modal.classList.toggle("is-open");
+
+  // Добавляем обработчики событий
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle("is-open");
   }
 })();
